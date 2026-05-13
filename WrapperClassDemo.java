6ab748838dbea5e1f7d7e74b26b3c234d08194b7
Ruchita Gor <117519687+RuchitaGor@users.noreply.github.com>
2026-05-13 11:06:07 +0530

    Add WrapperClassDemo to illustrate wrapper classes
    Demonstrate the use of wrapper classes in Java by converting a primitive int to an Integer object and displaying both values.

---
+++ WrapperClassDemo.java
@@ -0,0 +1,18 @@
+// Wrapper Class in Java example.
+
+public class WrapperClassDemo {
+
+    public static void main(String args[]) {
+
+        // Primitive data type
+        int num = 10;
+
+        // Converting primitive to object
+        Integer obj = Integer.valueOf(num);
+
+        // Display object value
+        System.out.println("Primitive Value : " + num);
+
+        System.out.println("Wrapper Object : " + obj);
+    }
+}
