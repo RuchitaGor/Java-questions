485a64ffede57197a53cd312fa38812e32348e88
Ruchita Gor <117519687+RuchitaGor@users.noreply.github.com>
2026-05-22 10:01:29 +0530

    Add Fibonacci series implementation in Java

---
+++ FibonacciSeries.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class FibonacciSeries
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n,a=0,b=1,c;
+
+        System.out.println("Enter Limit:");
+        n = sc.nextInt();
+
+        System.out.println("Fibonacci Series:");
+
+        for(int i=1;i<=n;i++)
+        {
+            System.out.print(a + " ");
+
+            c = a + b;
+
+            a = b;
+
+            b = c;
+        }
+    }
+}
